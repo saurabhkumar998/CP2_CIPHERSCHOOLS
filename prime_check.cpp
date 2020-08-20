db77e11277a7a699f390ec50fa1a11a439ec7119
saurabhkumar998 <49653295+saurabhkumar998@users.noreply.github.com>
2020-08-20 18:36:17 +0530

    Create prime_check.cpp

---
+++ prime_check.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int n;
+	cin>>n;
+
+	int i=2;
+	while(i<=n-1){
+		if(n%i==0){
+			cout<<"Not Prime";
+			break;
+			//return 0;
+		}
+		i = i + 1;
+	}
+	if(i==n){
+		cout<<"Prime";
+	}
+
+	return 0;
+}
